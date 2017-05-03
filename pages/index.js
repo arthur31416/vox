fcfa5041f3cab2cefd7697e81c8f7712b3694f85
Arthur Parmentier <arthur.jsx@gmail.com>
2017-05-03 21:06:22 +0200

    Added redux 2/n

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,9 @@ import Navbar from "../components/Navbar";
 import Tabbar from "../components/Tabbar";
 import EmptyStateHome from "../components/EmptyStateHome";
 import withData from "../lib/withData";
+import { bindActionCreators } from "redux";
+import { initStore, startClock, addCount, serverRenderClock } from "../store";
+import withRedux from "next-redux-wrapper";
 import { getUserId } from "../selectors";
 
 const NB_BOOKS = 0;
@@ -13,7 +16,7 @@ const NB_BOOKS = 0;
 const login = dispatch => () =>
   dispatch({ type: "LOGIN", payload: { id: 666, name: "Arthur" } });
 
-export default withData(props => (
+const Index = props => (
   <App>
     <Tabbar pathname={props.url.pathname} />
     <Navbar title={`Current books (${NB_BOOKS})`} />
@@ -23,5 +26,31 @@ export default withData(props => (
         login={login(props.dispatch)}
         isLogged={!!getUserId(props)}
       />}
+
+    <div>
+      You've come here {props.count} time{props.count > 1 ? "s" : ""}
+    </div>
   </App>
-));
+);
+
+Index.getInitialProps = ({ store, isServer }) => {
+  store.dispatch(serverRenderClock(isServer));
+  store.dispatch(addCount());
+
+  return { isServer };
+};
+
+const mapStateToProps = ({ count }) => ({
+  count
+});
+
+const mapDispatchToProps = dispatch => {
+  return {
+    addCount: bindActionCreators(addCount, dispatch),
+    startClock: bindActionCreators(startClock, dispatch)
+  };
+};
+
+export default withRedux(initStore, mapStateToProps, mapDispatchToProps)(
+  withData(Index)
+);
